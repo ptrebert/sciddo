8e98e8898f9b04c21efcaa59d8f72dc9ac35cd2e
Peter Ebert <pebert@mpi-inf.mpg.de>
2018-05-07 18:21:40 +0200

    ENH: added convenience scripts for develop installation

---
+++ misc/local/uninstall_sciddo.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm /TL/epigenetics2/work/pebert/conda/envs/sciddo/lib/python3.6/site-packages/scidlib.egg-link
+
+rm /TL/epigenetics2/work/pebert/conda/envs/sciddo/bin/sciddo.py
+
+rm -rf /home/pebert/work/code/github/sciddo/src/scidlib.egg-info
