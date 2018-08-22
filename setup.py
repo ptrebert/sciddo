cd4fccaedacaad4f1efc3623aadffbe81e5d1c38
Peter Ebert <36245614+ptrebert@users.noreply.github.com>
2018-08-22 10:13:32 +0200

    no stable fix for PyTables / HDF issue

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 # coding=utf-8
 
 import os as os
+import sys as sys
 
 from setuptools import setup, find_packages, Extension
-import numpy.distutils.misc_util as nputil
 
 
 def extract_package_info():
@@ -57,10 +57,28 @@ def locate_karlin_source():
     return relpath_script
 
 
+def locate_numpy_include_dirs():
+    """
+    :return:
+    """
+    try:
+        import numpy.distutils.misc_util as nputil
+        inc_dirs = [nputil.get_numpy_include_dirs()]
+    except ImportError:
+        # current hack-around, probably breakpoint
+        import site as site
+        site_pck = site.getsitepackages()
+        inc_dirs = []
+        for p in site_pck:
+            if os.path.isdir(p):
+                inc_dirs.append(os.path.join(p, 'numpy', 'core', 'include'))
+    return inc_dirs
+
+
 karlin_module = Extension(name='karlin',
                           sources=[locate_karlin_source()],
                           extra_compile_args=["-Wno-deprecated"],
-                          include_dirs=nputil.get_numpy_include_dirs(),
+                          include_dirs=locate_numpy_include_dirs(),
                           optional=False)
 
 pck_infos = extract_package_info()
@@ -70,12 +88,19 @@ setup(
     version=pck_infos['__version__'],
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    # removed this - no longer necessary
-    # since Karlin module was turned into
-    # a proper C extension
-    #package_data={'scidlib': ['karlin.so']},
     scripts=[locate_executable()],
     install_requires=read_requirements(),
+    # the pkgconfig requirement was added due to
+    # difficulties installing the PyTables dependency;
+    # according to the installation notes, recent versions
+    # of PyTables should be capable of finding the appropriate
+    # HDF include paths if pckconfig is available
+    # applies to Debian/Ubuntu systems
+    # this also causes trouble with Travis CI
+    # CPPFLAGS=-I/usr/include/hdf5/serial
+    # LDFLAGS=-L/usr/lib/x86_64-linux-gnu/hdf5/serial
+
+    setup_requires=['pkgconfig', 'numpy'],
     ext_modules=[karlin_module],
 
     author=pck_infos['__author__'],
